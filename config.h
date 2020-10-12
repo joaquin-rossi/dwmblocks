304deee69ee6c894d6e7084e825da49cdb95debf
joaquin-rossi <rossijoaquin1011@gmail.com>
2020-10-12 15:58:16 -0300

    Changed script names

--- config.h
+++ config.h
@@ -1,16 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"",	"music",	0,	11},*/
-	{"",	"internet",	1,	4},
-	{"",	"news",		0,	6},
-	/* {"",	"pacpackages",	0,	8}, */
-	/* {"",	"torrent",	20,	7}, */
-	{"",	"disk",		1,	0},
-	{"",	"battery",	1,	0},
-	/* {"",	"brightness",	1,	0}, */
-	{"",	"volume",	1,	3},
-	{"",	"silence",	1,	2},
+	{"",	"internet-statusbar",	1,	4},
+	{"",	"news-statusbar",		0,	6},
+	{"",	"disk-statusbar",		1,	0},
+	{"",	"battery-statusbar",	1,	0},
+	/* {"",	"brightness-statusbar",	1,	0}, */
+	{"",	"volume-statusbar",	1,	3},
+	{"",	"silence-statusbar",	1,	2},
 	{"",	"date +'%R %d/%m/%Y'",	60,	1},
 };
 

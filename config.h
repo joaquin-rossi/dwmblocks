5ec58c8d811867d64329816efb750b88043bb3c8
joaquin-rossi <joaquin@joaquinrossi.net>
2020-12-30 11:30:16 -0300

    command names

--- config.h
+++ config.h
@@ -2,12 +2,10 @@ static const Block blocks[] =
 {
 	/* icon command interval signal */
 	{" ", "public-ip", 1, 0},
-	{"", "stock", 60, 0},
-	{" ", "news", 60, 0},
+	{" ", "news", 0, 6},
 	{" ", "disk-usage /", 60, 0},
 	{" ", "disk-usage $HOME", 60, 0},
 	{"", "volume", 1, 1},
-	{"", "silence", 1, 2},
 	{"", "date +'%R %d/%m/%Y'",	5,	0},
 };
 
